5f7c08e634b855e775adf4336252857829cc8c7d
Lucas C Lyra Pereira <148347259+L-C-Lyra@users.noreply.github.com>
2025-04-12 15:49:18 -0300

    ✨ feat: "Aula 03 - 02-abr-2025"

--- Aula 03 - 02-abr-2025/02 - LAB - Exercicio 02/src/Main.java
+++ Aula 03 - 02-abr-2025/02 - LAB - Exercicio 02/src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int celsius, fahrenheit;
+        double celsius, fahrenheit;
 
-        System.out.print("Digite a Temperatura em Celsius: ");
+        System.out.println("Digite a Temperatura em Celsius: ");
         celsius = sc.nextInt();
 
-        fahrenheit = (int) (celsius * 1.8) + 32;
+        fahrenheit = (celsius * 1.8) + 32;
 
         System.out.println(celsius + "ºC Equivalem a " + fahrenheit + "ºF.");
     }
